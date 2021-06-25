cfee5d9d3164fdc73d9f8f1b5afc1fd76324c1af
Juanjo Salvador <juanjosalvador@netc.eu>
2021-06-25 18:23:34 +0200

    add version command option

--- src/dog.c
+++ src/dog.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-int main() {
-   printf("Woof!\n");
-   return 0;
+#include <stdlib.h>
+#include <unistd.h>
+#include "utils.h"
+
+int main(int argc, char *argv[]) {
+
+	int opt;
+	int s;
+	char buffer[32];
+
+	sprintf(buffer, "%s", whoof);
+
+	while ((opt = getopt(argc, argv, "v")) != -1) {
+			
+		switch (opt) {
+			case 'v': 
+				sprintf(buffer, "Dog, v%s", version); 
+				break;
+		}
+
+	}
+
+	printf("%s\n", buffer);
+
+	return 0;
 }
+
---
+++ src/utils.h
@@ -0,0 +1,5 @@
+#ifndef utils
+#define VERSION
+const char* whoof = "Whoof!";
+const char* version = "0.0.1";
+#endif
