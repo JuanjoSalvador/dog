9d6a1238670c11aac5be01b37030d2f97053ef1a
Juanjo Salvador <juanjosalvador@netc.eu>
2021-06-25 18:56:23 +0200

    add bork

--- src/dog.c
+++ src/dog.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "utils.h"
+#include "dogos.h"
 
 int main(int argc, char *argv[]) {
 
@@ -11,11 +12,14 @@ int main(int argc, char *argv[]) {
 
 	sprintf(buffer, "%s", whoof);
 
-	while ((opt = getopt(argc, argv, "v")) != -1) {
+	while ((opt = getopt(argc, argv, "bv")) != -1) {
 			
 		switch (opt) {
 			case 'v': 
 				sprintf(buffer, "Dog, v%s", version); 
+				break;
+			case 'b':
+				sprintf(buffer, "%s", bork);
 				break;
 		}
 
