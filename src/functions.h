c99115e6fcffb8a5fe1ebd0a3aadd26033a29b1e
J-Rios <jrios.github@gmail.com>
2021-10-01 23:33:21 +0200

    Avoid pointers to literal strings. Also, some changes and comments to be safe.

--- src/functions.h
+++ src/functions.h
@@ -8,6 +8,8 @@ void render(char* bark, int finish);
 void renderFile(char* filepath);
 char* paramsToString(int argc, char *argv[]);
 
+// A header file shouldn't implements functions, just define the function
+// prototype/interface (this function should be moved to dog.c)
 void clearScreen()
 {
   const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
